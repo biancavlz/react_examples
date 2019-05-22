01bd20fa3ce0451f9d61355aa6ed488c9d86be5a
Bianca <ing_biank@hotmail.com>
2019-05-22 12:37:05 +0200

    Adds a delete funtion and pass it as props to the todos component

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -5,11 +5,19 @@ class App extends Component {
     state = {
         todos: [{ id: 1, content: 'go shopping' }, { id: 2, content: 'clean the flat' }]
     }
+
+    deleteTodo = id => {
+        console.log(id)
+        const todos = this.state.todos.filter(todo => {
+            return todo.id !== id //return a new array excluding the todo with the id being clicked
+        })
+        this.setState({ todos }) //or this.setState({ todos: todos })
+    }
     render() {
         return (
             <div className="todo-app container">
                 <h1 className="center blue-text">Todo's</h1>
-                <Todos todos={this.state.todos} />
+                <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
             </div>
         )
     }
