7fb04fa67ab77646578bb7319c34acd0a9319a69
Bianca <ing_biank@hotmail.com>
2019-05-22 12:12:47 +0200

    adds a todos component

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import Todos from './Todos'
 
 class App extends Component {
     state = {
         todos: [{ id: 1, content: 'go shopping' }, { id: 2, content: 'clean the flat' }]
     }
     render() {
-        return <div className="App" />
+        return (
+            <div className="todo-app container">
+                <h1 className="center blue-text">Todo's</h1>
+                <Todos todos={this.state.todos} />
+            </div>
+        )
     }
 }
 
