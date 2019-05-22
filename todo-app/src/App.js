382208d9a5aaeff21ed026c68ca2ce834d6c39d5
Bianca <ing_biank@hotmail.com>
2019-05-22 11:43:54 +0200

    Adds initial state

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+    state = {
+        todos: [{ id: 1, content: 'go shopping' }, { id: 2, content: 'clean the flat' }]
+    }
     render() {
         return <div className="App" />
     }
