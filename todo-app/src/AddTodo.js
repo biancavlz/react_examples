980a3c0166fd117b7b253338b2af632d3887589e
Bianca <ing_biank@hotmail.com>
2019-05-23 11:22:46 +0200

    TODO APP: cleans the input field of a form after submission

--- todo-app/src/AddTodo.js
+++ todo-app/src/AddTodo.js
@@ -16,6 +16,12 @@ class AddTodo extends Component {
         e.preventDefault() //prevents the page being reloaded when submitting the form
         //console.log(this.state)
         this.props.addTodo(this.state) //function passed as prop
+
+        //resets the content to empty string,
+        //cleans the input value attribute after the form is being submitted
+        this.setState({
+            content: ''
+        })
     }
 
     render() {
@@ -23,7 +29,7 @@ class AddTodo extends Component {
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label>Add new todo</label>
-                    <input type="text" onChange={this.handleChange} />
+                    <input type="text" onChange={this.handleChange} value={this.state.content} />
                 </form>
             </div>
         )
